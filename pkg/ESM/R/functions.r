490ddd668dd25e905aeb9b7609c87c8bf855510f
timotheepoisot (Mr ) <tpoisot@um2.fr>
2009-12-12 18:14:29 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/esm@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ESM/R/functions.r
+++ pkg/ESM/R/functions.r
@@ -1,3 +1,11 @@
+get.DBF <- function(fit)
+{
+	fit <- sort(as.vector(fit),decreasing=TRUE)
+	test <- fit[2:length(fit)]
+	out <- sum(fit[1]-test)/length(test)
+	return(out)
+}
+
 get.HR <- function(pop)
 {
 	Ni <- sum(pop>0)
@@ -5,10 +13,10 @@ get.HR <- function(pop)
 	return(1-Ni/N)
 }
 
-get.SHANNON <- function(pop)
+get.HS <- function(pop)
 {
 	partiel <- NULL
-	pop <- as.vector(pop)+0.0000000001
+	pop <- as.vector(pop)+0.000000000001
 	for(i in 1:length(pop))
 	{
 		p <- pop[i]/sum(pop)
@@ -16,35 +24,6 @@ get.SHANNON <- function(pop)
 	}
 	shannon <- ifelse(length(pop)<=1,0,-sum(partiel) / log(length(pop)))
 	return(1-shannon)
-}
-
-get.DFP <- function(fit)
-{
-	score <- NULL
-	fit <- as.vector(fit)
-	fit <- na.exclude(sort(fit,decreasing=TRUE))
-	vecdiff <- NULL	
-	for(i in 1:(length(fit)-1))
-	{
-		vecdiff[i] <- (fit[i+1]-fit[i])+1
-	}
-	score <- prod(abs(vecdiff))
-	return(1-score)
-}
-
-get.WADFP <- function(fit)
-{
-	ngen <- length(fit)
-	sorted <- sort(fit,decreasing=TRUE)
-	tempS <- NULL
-	correc <- (ngen-1)/ngen
-	for(g in 1:(ngen-1))
-	{
-		tempS[g] <- (1+sorted[g]-sorted[g+1])*sorted[g]
-	}
-	S <- sum(tempS)/sum(fit)
-	S <- (S-correc)/(2-correc)
-	return(S)
 }
 
 get.SSI <- function(occup)
@@ -57,33 +36,22 @@ get.SSI <- function(occup)
 	return(SSI)
 }
 
-dfp <- function(m)
+dbf <- function(m)
 {
 	spe <- vector('numeric',length=nrow(m))
 	for(i in 1:nrow(m))
 	{
-		spe[i] <- get.DFP(m[i,])
+		spe[i] <- get.DBF(m[i,])
 	}
 	return(spe)
 }
 
-wadfp <- function(m)
+hs <- function(m)
 {
 	spe <- vector('numeric',length=nrow(m))
 	for(i in 1:nrow(m))
 	{
-		spe[i] <- get.WADFP(m[i,])
-	}
-	return(spe)
-}
-
-
-shannon <- function(m)
-{
-	spe <- vector('numeric',length=nrow(m))
-	for(i in 1:nrow(m))
-	{
-		spe[i] <- get.SHANNON(as.vector(as.numeric(m[i,])))
+		spe[i] <- get.HS(as.vector(as.numeric(m[i,])))
 	}
 	return(spe)
 }
@@ -115,3 +83,6 @@ getspe <- function(mat,measure=hr)
 	names(out) <- rownames(mat)
 	return(out)
 }
+
+cv		= function(d) sd(d)/mean(d)
+last	= function(d) d[length(d)]
