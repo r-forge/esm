6cb48832abc464a1ddaf93d22b5672aae30ce5b4
timotheepoisot (Mr ) <tpoisot@um2.fr>
2011-05-29 17:24:36 +0000

    bug in bip2n3d
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/esm@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ESM/R/bip2n3d.r
+++ pkg/ESM/R/bip2n3d.r
@@ -11,9 +11,3 @@ bip2n3d = function(mat,filename='bip2n3d')
 	}
 	write.table(n3d,file=paste(filename,'.web',sep=''),row.names=FALSE,col.names=FALSE)
 }
-
-bip2n3d(Sweb[[1]],filename='w1')
-bip2n3d(Sweb[[2]],filename='w2')
-bip2n3d(Sweb[[3]],filename='w3')
-bip2n3d(Sweb[[4]],filename='w4')
-bip2n3d(Sweb[[5]],filename='w5')
